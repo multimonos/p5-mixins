6e13312a7582631043ea2f0de6cc5b5948295f4b
Craig Hopgood <searaig@gmail.com>
2021-02-20 13:42:01 -0500

    Added vvertex(vec, u, v) to VectorDrawing.js

--- src/VectorDrawing.js
+++ src/VectorDrawing.js
@@ -6,4 +6,5 @@ const VectorDrawing = p => {
     p.vrect = ( a, b ) => p.rect( a.x, a.y, b.x - a.x, b.y - a.y )
     p.vcircle = size => vec => p.circle( vec.x, vec.y, size )
     p.vtranslate = vec => p.translate( vec.x, vec.y, vec.z )
+    p.vvertex = ( vec, u, v ) => p.vertex( vec.x, vec.y, vec.z, u, v )
 }
