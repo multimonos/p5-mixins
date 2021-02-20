078e03576c1066bfaaa63297ab6bff46aa0b7057
Craig Hopgood <searaig@gmail.com>
2021-02-20 13:25:48 -0500

    remove vector manipulation methods

--- src/VectorDrawing.js
+++ src/VectorDrawing.js
@@ -4,14 +4,6 @@ export { VectorDrawing as default }
 
 const VectorDrawing = p => {
     p.vrect = ( a, b ) => p.rect( a.x, a.y, b.x - a.x, b.y - a.y )
-    p.vcircle = a => size => p.circle( a.x, a.y, size )
-    p.vtranslate = a => p.translate( a.x, a.y )
-    p.vadd = ( a, b ) => p5.Vector.add( a, b )
-    p.vcopy = a => a.copy()
-    p.vmult = ( a, b ) => p5.Vector.mult( a, b )
-    p.vzero = () => new p5.Vector( 0, 0, 0 )
-    p.vnull = () => p.vzero()
-    p.vi2 = () => new p5.Vector( 1, 1, 0 )
-    p.vi3 = () => new p5.Vector( 1, 1, 1 )
-    p.vcreate = ( ...args ) => new p5.Vector( ...args )
+    p.vcircle = size => vec => p.circle( vec.x, vec.y, size )
+    p.vtranslate = vec => p.translate( vec.x, vec.y, vec.z )
 }
