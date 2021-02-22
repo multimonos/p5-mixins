b56dbb093195e83b73cdcee5f882c04488bca693
Craig Hopgood <searaig@gmail.com>
2021-02-22 16:28:04 -0500

    added the vline(a, b) vector fn

--- src/VectorDrawing.js
+++ src/VectorDrawing.js
@@ -5,6 +5,7 @@ export { VectorDrawing as default }
 const VectorDrawing = p => {
     p.vrect = ( a, b ) => p.rect( a.x, a.y, b.x - a.x, b.y - a.y )
     p.vcircle = size => vec => p.circle( vec.x, vec.y, size )
+    p.vline = ( a, b ) => p.line( a.x, a.y, b.x, b.y )
     p.vtranslate = vec => p.translate( vec.x, vec.y, vec.z )
     p.vvertex = ( vec, u, v ) => p.vertex( vec.x, vec.y, vec.z, u, v )
 }
